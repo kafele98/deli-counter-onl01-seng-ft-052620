d5ac2399e77305a5d23b8f4afcf55b1fbfadd6fe
Zoe Keller <keller.zoe98@gmail.com>
2020-06-09 14:59:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,25 @@
+katz_deli = []
 
-def line(array) 
-  if array.length >= 1
-    nuarray = []
-    counter = 1 
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{nuarray.join(" ")}"
-  else
+def line(x)
+  line_array = []
+  if x.length == 0
     puts "The line is currently empty."
-  end
-end
-
-
-def take_a_number(line, new_person)
-  line.push(new_person) 
-  puts "Welcome, #{new_person}. You are number #{line.length} in line."
-end
-
-take_a_number(katz_deli, "Fyvish")
-
-def now_serving(line)
-  if line.length == 0
-    puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}." 
-    line.shift 
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
   end
 end
-
-puts now_serving(katz_deli)
-puts katz_deli
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
