a7f427432587010bd8633c7860083608478ece56
Zoe Keller <keller.zoe98@gmail.com>
2020-06-09 14:58:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,17 @@
 
+def line(array) 
+  if array.length >= 1
+    nuarray = []
+    counter = 1 
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
 
 
 def take_a_number(line, new_person)
